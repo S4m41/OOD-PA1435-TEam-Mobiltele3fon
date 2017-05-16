8bc39caed3b14395c3b15769732210919605b210
Samai_L <Samai96@live.se>
2017-05-16 18:11:22 +0200

    Comments in inventory

--- DungeonTwitterGame/DungeonTwitterGame/Inventory.hpp
+++ DungeonTwitterGame/DungeonTwitterGame/Inventory.hpp
@@ -5,6 +5,7 @@ class Inventory
 public:
 	Inventory();
 	~Inventory();
+	//returns slotid of item added
 	int addItem(Item* itemToAdd);
 	/*disowns item caller beware!, 
 	only checks pointer not object, removes first pointer encoutered not caring for duplicates
@@ -13,6 +14,7 @@ public:
 	/*disowns item caller beware!,
 	returns false if no item in slot*/
 	bool dropItem(int slotID);
+	//returns non null item in slotid or null if slot emty
 	Item* itemInSlot(int slotID);
 protected:
 
