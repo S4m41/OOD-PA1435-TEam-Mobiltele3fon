d37d3a084a8fc236af45c279632e1a55c0751940
Samai_L <Samai96@live.se>
2017-05-16 20:40:08 +0200

    Linker problems

--- DungeonTwitterGame/DTGTests/UnitTest.cpp
+++ DungeonTwitterGame/DTGTests/UnitTest.cpp
@@ -1,5 +1,5 @@
 ﻿#include "stdafx.h"
-#include "Item.hpp"
+#include "Weapon.hpp"
 using namespace System;
 using namespace System::Text;
 using namespace System::Collections::Generic;
@@ -11,52 +11,13 @@ namespace DTGTests
 	[TestClass]
 	public ref class UnitTest
 	{
-	private:
-		//TestContext^ testContextInstance;
-
-	public: 
-		///// <summary>
-		/////Gets or sets the test context which provides
-		/////information about and functionality for the current test run.
-		/////</summary>
-		//property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ TestContext
-		//{
-		//	Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ get()
-		//	{
-		//		return testContextInstance;
-		//	}
-		//	System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ value)
-		//	{
-		//		testContextInstance = value;
-		//	}
-		//};
-
-		//#pragma region Additional test attributes
-		////
-		////You can use the following additional attributes as you write your tests:
-		////
-		////Use ClassInitialize to run code before running the first test in the class
-		////[ClassInitialize()]
-		////static void MyClassInitialize(TestContext^ testContext) {};
-		////
-		////Use ClassCleanup to run code after all tests in a class have run
-		////[ClassCleanup()]
-		////static void MyClassCleanup() {};
-		////
-		////Use TestInitialize to run code before running each test
-		////[TestInitialize()]
-		////void MyTestInitialize() {};
-		////
-		////Use TestCleanup to run code after each test has run
-		////[TestCleanup()]
-		////void MyTestCleanup() {};
-		////
-		//#pragma endregion 
-
+	public:
+	
 		[TestMethod]
 		void TestMethod1()
 		{
-			Assert::AreEqual(1, 0);
+			char* c = "Axe.png";
+			Weapon w = Weapon(c);
 			//
 			// TODO: Add test logic here
 			//
